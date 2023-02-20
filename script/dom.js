a9ec371142a6759cb7da5f82aefa837a8b41f57e
Willi4mL <wi.lindbolm@gmail.com>
2023-02-20 22:49:10 +0100

    created divs and put names in it

--- script/dom.js
+++ script/dom.js
@@ -1,18 +1,50 @@
-import { pokemonUrl } from "./fetch.js"
+import { pokemonUrl, pokemonAbility, pokemonStats } from "./fetch.js"
 
 const findChampionButton = document.querySelector('#findChampion')
 const myTeamButton = document.querySelector('#myTeam')
 const searchPokemonNav = document.querySelector('#contentSearch')
 const searchPokemonInput = document.querySelector('#searchChampion')
+const cardContainer = document.querySelector('#pokemonCardList')
 
 // Visible or invisible 
 const invisible = 'none'
 const visible = 'block'
 
+// PokemonStats
+function pokemonStatFunction() {
+	let stat = {
+		image: document.createElement('img'),
+		name: document.createElement('p'),
+		ability: document.createElement('p'),
+		stats: document.createElement('p')
+	}
+	return stat
+}
 
 // FindChampion button
-findChampionButton.addEventListener('click', () => {
+findChampionButton.addEventListener('click', async () => {
 	searchPokemonNav.style.display = visible
+	const findUrl = pokemonUrl
+	const abilityUrl = pokemonAbility
+	const statsUrl = pokemonStats
+
+	let response = await fetch(findUrl)
+	let data = await response.json()
+
+	// Creates div inside li for every pokemon
+	const allPokemons = data.results.filter(allPokemon => allPokemon.name)
+	allPokemons.forEach(allPokemon => {
+		console.log(allPokemon.name)
+
+		let pokemonStat = pokemonStatFunction()
+		let pokemonCard = document.createElement('div')
+		pokemonCard.setAttribute('class', 'pokemon-card')
+
+		pokemonStat.name.innerText = JSON.stringify(allPokemon.name)
+		pokemonCard.append(pokemonStat.name)
+		cardContainer.append(pokemonCard)
+
+	})
 })
 
 // MyTeam button
@@ -24,20 +56,20 @@ myTeamButton.addEventListener('click', () => {
 searchPokemonInput.addEventListener('keypress', async (event) => {
 	if (event.key === 'Enter') {
 		const searchString = searchPokemonInput.value;
-		const url = pokemonUrl;
+		const url = pokemonUrl
 
 		try {
-			let response = await fetch(url);
-			let data = await response.json();
-			console.log(data);
+			let response = await fetch(url)
+			let data = await response.json()
+			console.log(data)
 
-			const matchingPokemon = data.results.filter(pokemon => pokemon.name.includes(searchString));
+			const matchingPokemon = data.results.filter(pokemon => pokemon.name.includes(searchString))
 			matchingPokemon.forEach(pokemon => {
-				console.log(pokemon.name);
+				console.log(pokemon.name)
 			});
 
 		} catch (error) {
-			console.log('Felmeddelande', error.message);
+			console.log('Felmeddelande', error.message)
 		}
 	}
 });
--- script/fetch.js
+++ script/fetch.js
@@ -1,3 +1,7 @@
 const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon'
+const pokemonAbility = 'https://pokeapi.co/api/v2/ability/'
+const pokemonStats = 'https://pokeapi.co/api/v2/stat/'
 
 export {pokemonUrl}
+export {pokemonAbility}
+export {pokemonStats}
