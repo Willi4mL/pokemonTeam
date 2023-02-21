37df49dcf21f964fac227103a7abee49a6fb540e
Willi4mL <wi.lindbolm@gmail.com>
2023-02-21 23:08:48 +0100

    added new key in localstorage

--- script/dom.js
+++ script/dom.js
@@ -1,106 +1,119 @@
-import { pokemonUrl, pokemonAbility, pokemonStats } from "./fetch.js"
+import { pokemonUrl } from "./fetch.js"
 
 const findChampionButton = document.querySelector('#findChampion')
 const myTeamButton = document.querySelector('#myTeam')
 const searchPokemonNav = document.querySelector('#contentSearch')
 const searchPokemonInput = document.querySelector('#searchChampion')
 const cardContainer = document.querySelector('#pokemonCardList')
+const buttonCard = document.querySelector('#buttonCard')
 
 
 // Visible or invisible 
 const invisible = 'none'
 const visible = 'block'
 
-// PokemonStats
-function pokemonStatFunction() {
-	let stat = {
-		image: document.createElement('img'),
-		name: document.createElement('p'),
-		ability: document.createElement('p'),
-		stats: document.createElement('p')
-	}
-	return stat
-}
-
-
-
 // FindChampion button
 findChampionButton.addEventListener('click', async () => {
 	searchPokemonNav.style.display = visible
 	const findUrl = pokemonUrl
-	const abilityUrl = pokemonAbility
-	const statsUrl = pokemonStats
 
 	let response = await fetch(findUrl)
 	let data = await response.json()
 
-fetchPokemon()
+	fetchPokemon()
 })
 
-// MyTeam button
-myTeamButton.addEventListener('click', () => {
-	searchPokemonNav.style.display = invisible
-})
-
-// LocalStorage list
+// LocalStorage list and display divs
 let pokemonList = []
 const fetchPokemon = () => {
-    const baseUrl = pokemonUrl;
-    const pokemonList = [];
-  
-    for (let i = 1; i <= 151; i++) {
-      const url = `${baseUrl}${i}`;
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          const pokemon = {
-            name: data.name,
-            image: data.sprites['front_default'],
-            type: data.types.map(type => type.type.name).join(', '),
-			abilites: data.abilities.map(ability => ability.ability.name).join(', ')
-		};
+	const baseUrl = pokemonUrl;
+	const pokemonList = [];
 
-			const pokemonDiv = document.createElement('div');
-			pokemonDiv.setAttribute('class', 'div-card')
-			pokemonDiv.innerHTML = `
-			  <h3>${pokemon.name}</h3>
+	for (let i = 1; i <= 151; i++) {
+		const url = `${baseUrl}${i}`;
+		fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				const pokemon = {
+					name: data.name,
+					image: data.sprites['front_default'],
+					type: data.types.map(type => type.type.name).join(', '),
+					abilites: data.abilities.map(ability => ability.ability.name).join(', ')
+				};
+				const pokemonDiv = document.createElement('button');
+				pokemonDiv.setAttribute('class', 'button-card')
+				pokemonDiv.setAttribute('id', 'buttonCard')
+				pokemonDiv.innerHTML = `
+			  <h2>${pokemon.name}</h2>
 			  <img src="${pokemon.image}" alt="${pokemon.name}">
 			  <p>Type: ${pokemon.type}</p>
 			  <p>Ability: ${pokemon.abilites}</p>
 			`;
-			cardContainer.appendChild(pokemonDiv);
-          
-          pokemonList.push(pokemon);
- 
-          // Save the list to local storage
-          localStorage.setItem('pokemonList', JSON.stringify(pokemonList));
-        })
-        .catch(error => console.error(error));
-    }
-  };
+				cardContainer.append(pokemonDiv);
+
+				pokemonList.push(pokemon);
+
+				// Save the list to local storage
+				localStorage.setItem('pokemonList', JSON.stringify(pokemonList));
+			})
+			.catch(error => console.error(error));
+	}
+};
 fetchPokemon()
 
-  searchPokemonInput.addEventListener('keypress', async (event) => {
+// Search pokemon and display card
+searchPokemonInput.addEventListener('keypress', async (event) => {
 	if (event.key === 'Enter') {
-	  const searchString = searchPokemonInput.value;
-	  pokemonList = JSON.parse(localStorage.getItem('pokemonList'));
-  
-	  const matchingPokemon = pokemonList.filter(pokemon => pokemon.name.includes(searchString));
-  
-	  // Clear the existing content of the container
-	  cardContainer.innerHTML = '';
-  
-	  // Create a new div for each matching pokemon and append it to the container
-	  matchingPokemon.forEach(pokemon => {
-		const pokemonDiv = document.createElement('div');
-		pokemonDiv.setAttribute('class', 'div-card')
-		pokemonDiv.innerHTML = `
+		const searchString = searchPokemonInput.value;
+		pokemonList = JSON.parse(localStorage.getItem('pokemonList'));
+
+		const matchingPokemon = pokemonList.filter(pokemon => pokemon.name.includes(searchString));
+
+		// Clear the existing content of the container
+		cardContainer.innerHTML = '';
+
+		// Create a new div for each matching pokemon and append it to the container
+		matchingPokemon.forEach(pokemon => {
+			const pokemonDiv = document.createElement('button');
+			pokemonDiv.setAttribute('class', 'button-card')
+			pokemonDiv.setAttribute('id', 'buttonCard')
+			pokemonDiv.innerHTML = `
 		  <h2>${pokemon.name}</h2>
 		  <img src="${pokemon.image}" alt="${pokemon.name}">
 		  <p>Type: ${pokemon.type}</p>
 		`;
-		cardContainer.appendChild(pokemonDiv);
-	  });
+			cardContainer.appendChild(pokemonDiv);
+		});
 	}
-  });
- 
+});
+
+//   Add pokemons to my team in localstorage
+let myTeamList = JSON.parse(localStorage.getItem('myTeamList')) || {};
+
+document.addEventListener('click', (event) => {
+  if (event.target.id === 'buttonCard') {
+    const pokemonList = JSON.parse(localStorage.getItem('pokemonList'));
+    const pokemon = pokemonList.find(pokemon => pokemon.name === event.target.querySelector('h2').textContent);
+  
+    // Clear the existing data in myTeamList
+    myTeamList = {};
+    
+    // Add the pokemon object to myTeamList object using the pokemon name as the key
+
+		myTeamList[pokemon.name] = pokemon;
+		
+		// Save the updated myTeamList to localStorage
+		localStorage.setItem('myTeamList', JSON.stringify(myTeamList));
+  }
+});
+
+
+// MyTeam button
+myTeamButton.addEventListener('click', () => {
+	searchPokemonNav.style.display = invisible
+
+	// Clear content of the container
+	cardContainer.innerHTML = '';
+
+})
+
--- script/fetch.js
+++ script/fetch.js
@@ -1,7 +1,4 @@
 const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/'
-const pokemonAbility = 'https://pokeapi.co/api/v2/ability/'
-const pokemonStats = 'https://pokeapi.co/api/v2/stat/'
+
 
 export {pokemonUrl}
-export {pokemonAbility}
-export {pokemonStats}
