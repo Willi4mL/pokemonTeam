a8211b60a0af4f856080e50882fdc17b6bed828e
Willi4mL <wi.lindbolm@gmail.com>
2023-02-23 22:10:31 +0100

    correct color around cards

--- script/dom.js
+++ script/dom.js
@@ -19,10 +19,27 @@ myTeamSection.style.display = invisible
 reserveSection.style.display = invisible
 
 // LocalStorage list and display divs
-let pokemonList = []
+const typeColors = {
+	grass: '#78c850',
+	fire: '#f08030',
+	water: '#6890f0',
+	bug: '#a8b820',
+	normal: '#a8a878',
+	poison: '#a040a0',
+	ground: '#e0c068',
+	fighting: '#c03028',
+	fairy: '#ee99ac',
+	psychic: '#f85888',
+	ghost: '#705898',
+	ice: '#98d8d8',
+	dragon: '#7038f8',
+	electric: '#f8d030',
+	rock: '#b8a038'
+};
+let pokemonList = [];
 const fetchPokemon = () => {
 	const baseUrl = pokemonUrl;
-	const pokemonList = [];
+	let pokemonList = [];
 
 	for (let i = 1; i <= 151; i++) {
 		const url = `${baseUrl}${i}`;
@@ -32,80 +49,33 @@ const fetchPokemon = () => {
 				const pokemon = {
 					name: data.name,
 					image: data.sprites['front_default'],
-					type: data.types.map(type => type.type.name).join(', '),
-					abilites: data.abilities.map(ability => ability.ability.name).join(', ')
-
+					type: data.types.map(type => type.type.name),
+					abilities: data.abilities.map(ability => ability.ability.name)
 				};
-
-				
-
-
 				const pokemonDiv = document.createElement('div');
-				pokemonDiv.setAttribute('class', 'div-card')
-				pokemonDiv.setAttribute('id', 'divCard')
+				pokemonDiv.setAttribute('class', 'div-card');
+				pokemonDiv.setAttribute('id', 'divCard');
 				pokemonDiv.innerHTML =
 					`<h2 class="card-name" id="cardHeading">${pokemon.name}</h2>
-				<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
-				<p class="card-type" id="cardType">Type: ${pokemon.type}</p>
-				<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilites}</p> `
+			<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
+			<p class="card-type" id="cardType">Type: ${pokemon.type.join(', ')}</p>
+			<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilities.join(', ')}</p>`;
 
-				const pokemonCardButton = document.createElement('button')
-				pokemonCardButton.setAttribute('class', 'card-button')
-				pokemonCardButton.setAttribute('id', 'cardButton')
-				pokemonCardButton.innerText = 'Add to my team'
+				const pokemonCardButton = document.createElement('button');
+				pokemonCardButton.setAttribute('class', 'card-button');
+				pokemonCardButton.setAttribute('id', 'cardButton');
+				pokemonCardButton.innerText = 'Add to my team';
 
-				const pokemonCardButtonReserve = document.createElement('button')
-				pokemonCardButtonReserve.setAttribute('class', 'card-button-reserve')
-				pokemonCardButtonReserve.setAttribute('id', 'cardButtonReserve')
-				pokemonCardButtonReserve.innerText = 'Add to reserve'
+				const pokemonCardButtonReserve = document.createElement('button');
+				pokemonCardButtonReserve.setAttribute('class', 'card-button-reserve');
+				pokemonCardButtonReserve.setAttribute('id', 'cardButtonReserve');
+				pokemonCardButtonReserve.innerText = 'Add to reserve';
 
-				if (pokemon.type.includes('grass')) {
-					pokemonDiv.style.border = '10px solid #78c850';
-				  }
-				  if (pokemon.type.includes('fire')) {
-					pokemonDiv.style.border = '10px solid #f08030';
-				  }
-				  if (pokemon.type.includes('water')) {
-					pokemonDiv.style.border = '10px solid #6890f0';
-				  }
-				  if (pokemon.type.includes('bug')) {
-					pokemonDiv.style.border = '10px solid #a8b820';
-				  }
-				  if (pokemon.type.includes('normal')) {
-					pokemonDiv.style.border = '10px solid #a8a878';
-				  }
-				  if (pokemon.type.includes('poison')) {
-					pokemonDiv.style.border = '10px solid #a040a0';
-				  }
-				  if (pokemon.type.includes('electric')) {
-					pokemonDiv.style.border = '10px solid #f8d030';
-				  }
-				  if (pokemon.type.includes('ground')) {
-					pokemonDiv.style.border = '10px solid #e0c068';
-				  }
-				  if (pokemon.type.includes('fairy')) {
-					pokemonDiv.style.border = '10px solid #ee99ac';
-				  }
-				  if (pokemon.type.includes('fighting')) {
-					pokemonDiv.style.border = '10px solid #c03028';
-				  }
-				  if (pokemon.type.includes('psychic')) {
-					pokemonDiv.style.border = '10px solid #f85888';
-				  }
-				  if (pokemon.type.includes('rock')) {
-					pokemonDiv.style.border = '10px solid #b8a038';
-				  }
-				  if (pokemon.type.includes('ghost')) {
-					pokemonDiv.style.border = '10px solid #705898';
-				  }
-				  if (pokemon.type.includes('ice')) {
-					pokemonDiv.style.border = '10px solid #98d8d8';
-				  }
-				  if (pokemon.type.includes('dragon')) {
-					pokemonDiv.style.border = '10px solid #7038f8';
-				  }
-
-				
+				// Find righgt color for the border
+				const firstType = pokemon.type.find(type => type in typeColors);
+				if (firstType) {
+					pokemonDiv.style.border = `10px solid ${typeColors[firstType]}`;
+				}
 
 				pokemonDiv.append(pokemonCardButton, pokemonCardButtonReserve)
 				cardContainer.append(pokemonDiv)
@@ -139,9 +109,9 @@ searchPokemonInput.addEventListener('input', () => {
 		pokemonDiv.setAttribute('id', 'divCard')
 		pokemonDiv.innerHTML =
 		`<h2 class="card-name" id="cardHeading">${pokemon.name}</h2>
-			<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
-				<p class="card-type" id="cardType">Type: ${pokemon.type}</p>
-				<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilites}</p> `
+		<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
+		<p class="card-type" id="cardType">Type: ${pokemon.type}</p>
+		<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilities.join(', ')}</p>`;
 
 		const pokemonCardButton = document.createElement('button')
 		pokemonCardButton.setAttribute('class', 'card-button')
@@ -157,8 +127,14 @@ searchPokemonInput.addEventListener('input', () => {
 		cardButtonDiv.setAttribute('class', 'button-div')
 		cardButtonDiv.setAttribute('id', 'buttonDiv')
 
+		
 		pokemonDiv.append(pokemonCardButton, pokemonCardButtonReserve)
 		cardContainer.append(pokemonDiv);
+		// Find righgt color for the border
+		const firstType = pokemon.type.find(type => type in typeColors);
+		if (firstType) {
+			pokemonDiv.style.border = `10px solid ${typeColors[firstType]}`;
+		}
 	});
 });
 
@@ -178,10 +154,10 @@ findChampionButton.addEventListener('click', () => {
 		pokemonDiv.setAttribute('class', 'div-card')
 		pokemonDiv.setAttribute('id', 'divCard')
 		pokemonDiv.innerHTML =
-		`<h2 class="card-name" id="cardHeading">${pokemon.name}</h2>
+			`<h2 class="card-name" id="cardHeading">${pokemon.name}</h2>
 			<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
 			<p class="card-type" id="cardType">Type: ${pokemon.type}</p>
-			<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilites}</p> `
+			<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilities.join(', ')}</p>`;
 
 		const pokemonCardButton = document.createElement('button')
 		pokemonCardButton.setAttribute('class', 'card-button')
@@ -196,6 +172,12 @@ findChampionButton.addEventListener('click', () => {
 		const cardButtonDiv = document.createElement('div')
 		cardButtonDiv.setAttribute('class', 'button-div')
 		cardButtonDiv.setAttribute('id', 'buttonDiv')
+
+		// Find righgt color for the border
+		const firstType = pokemon.type.find(type => type in typeColors);
+		if (firstType) {
+			pokemonDiv.style.border = `10px solid ${typeColors[firstType]}`;
+		}
 
 		pokemonDiv.append(pokemonCardButton, pokemonCardButtonReserve)
 		cardContainer.append(pokemonDiv);
@@ -295,19 +277,26 @@ function addMyTeam() {
 	let myTeamList = JSON.parse(localStorage.getItem('myTeamList')) || {};
 	let completeTeamDisplayed = false;
 	Object.values(myTeamList).forEach(pokemon => {
+
 		const pokemonDiv = document.createElement('div');
 		pokemonDiv.setAttribute('class', 'div-card');
 		pokemonDiv.setAttribute('id', 'divCard');
 		pokemonDiv.innerHTML =
-		`<h2 class="card-name" id="cardHeading">${pokemon.name}</h2>
+			`<h2 class="card-name" id="cardHeading">${pokemon.name}</h2>
 				<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
 				<p class="card-type" id="cardType">Type: ${pokemon.type}</p>
-				<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilites}</p> `
+				<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilities.join(', ')}</p>`;
 
 		const pokemonCardButtonRemove = document.createElement('button')
 		pokemonCardButtonRemove.setAttribute('class', 'card-button-remove')
 		pokemonCardButtonRemove.setAttribute('id', 'cardButtonRemove')
 		pokemonCardButtonRemove.innerText = 'Remove'
+
+			// Find righgt color for the border
+			const firstType = pokemon.type.find(type => type in typeColors);
+			if (firstType) {
+				pokemonDiv.style.border = `10px solid ${typeColors[firstType]}`;
+			}
 
 		// If my team equals 3 display complete team text
 		if (myTeamList.length === 3) {
@@ -337,14 +326,20 @@ function addMyReserve() {
 		pokemonDiv.setAttribute('id', 'divCard');
 		pokemonDiv.innerHTML =
 		`<h2 class="card-name" id="cardHeading">${pokemon.name}</h2>
-			<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
-			<p class="card-type" id="cardType">Type: ${pokemon.type}</p>
-			<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilites}</p> `
+				<img class="card-img" id="cardImg" src="${pokemon.image}" alt="${pokemon.name}">
+				<p class="card-type" id="cardType">Type: ${pokemon.type}</p>
+				<p class="card-ability" id="cardAbility">Ability: ${pokemon.abilities.join(', ')}</p>`;
 
 		const pokemonCardButtonRemove = document.createElement('button')
 		pokemonCardButtonRemove.setAttribute('class', 'card-button-remove')
 		pokemonCardButtonRemove.setAttribute('id', 'cardButtonRemove')
 		pokemonCardButtonRemove.innerText = 'Remove'
+
+		// Find righgt color for the border
+		const firstType = pokemon.type.find(type => type in typeColors);
+		if (firstType) {
+			pokemonDiv.style.border = `10px solid ${typeColors[firstType]}`;
+		}
 
 		pokemonDiv.append(pokemonCardButtonRemove)
 
